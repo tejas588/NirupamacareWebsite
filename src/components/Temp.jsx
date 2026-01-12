10fd2b29bc75dc4783799d3743c4f7433675587d
tejas588 <saitejasrepala@gmail.com>
2026-01-12 11:49:29 +0530

    Temporary rename

--- src/components/Temp.jsx
+++ src/components/Temp.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { User, MapPin, Calendar, Users, ArrowLeft, Edit3, ShieldCheck, Activity } from 'lucide-react';
 import { api } from '../api';
-import './UserprofileView.css'; 
+import './UserProfileView.css'; 
 
 const UserProfileView = () => {
   const navigate = useNavigate();
